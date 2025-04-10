8ea5eae101dff12796719b905c9c0d7b91813efc
Kawtar <kawtartaik123@gmail.com>
2025-04-10 11:56:16 +0100

    connect to postgres railway instance and setup env variables for DB username + password

--- backend/src/main/java/com/cosmic/tracker/TrackerApplication.java
+++ backend/src/main/java/com/cosmic/tracker/TrackerApplication.java
@@ -1,17 +1,7 @@
 package com.cosmic.tracker;
 
-import com.cosmic.tracker.cosmicevent.CosmicEvent;
-import com.cosmic.tracker.cosmicevent.CosmicEventRepository;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.io.InputStream;
-import java.util.List;
 
 @SpringBootApplication
 public class TrackerApplication {

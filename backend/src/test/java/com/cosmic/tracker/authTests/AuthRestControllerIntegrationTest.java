4133f4e73c14b218d203996dc92d239e8987f410
kei077 <kawtartaik123@gmail.com>
2025-05-21 03:24:00 +0100

    add testpropertysource annotation to tests for dummy nasa api key

--- backend/src/test/java/com/cosmic/tracker/authTests/AuthRestControllerIntegrationTest.java
+++ backend/src/test/java/com/cosmic/tracker/authTests/AuthRestControllerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.security.Key;
@@ -28,6 +29,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "NASA_API_KEY=dummy-key-for-testing"
+})
 class AuthRestControllerIntegrationTest {
 
     @Autowired
